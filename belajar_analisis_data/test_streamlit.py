c8412a77f5e8bc3c866dc372c38a29c297ababa7
AndikaBN <bintangnursalih275@gmail.com>
2024-10-01 08:00:11 +0800

    menuju dashbord

---
+++ belajar_analisis_data/test_streamlit.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title(
+ """
+ # Example of Streamlit
+  Hello, para calon praktisi data masa depan!
+
+  saya adalah seorang data scientist yang sedang belajar Streamlit.
+ """
+)
